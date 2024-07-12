e1c6c831cec411885a501145db110457c5b1c041
Omar Haji <s12029055@stu.najah.edu>
2024-07-12 22:16:18 +0300

    WIP

---
+++ server.js
@@ -0,0 +1,26 @@
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// const port = 8000;
+
+// // Serve static files from the current directory (myWeb)
+// app.use(express.static(path.join(__dirname)));
+
+// // Serve images from the "images" directory
+// app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'index.html'));
+// });
+
+// app.get('/about', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'about.html'));
+// });
+
+// app.get('/contact', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'contact.html'));
+// });
+
+// app.listen(port, () => {
+//     console.log(`Server is running at http://localhost:${port}`);
+// });
