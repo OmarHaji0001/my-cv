58b77c02bdbe14c39f04c4f8a4dfee847baf76e8
Omar Haji <s12029055@stu.najah.edu>
2024-07-19 17:09:05 +0300

    organzing css folder

--- JAVASCRIPTs/footer.js
+++ JAVASCRIPTs/footer.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch('footer.html')
+    fetch('../HTMLs/footer.html')
         .then(response => {
             if (!response.ok) {
                 throw new Error('HTTP error! Status: ' + response.status);
--- JAVASCRIPTs/mainnav.js
+++ JAVASCRIPTs/mainnav.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch('mainnav.html')
+    fetch('../HTMLs/mainnav.html')
         .then(response => {
             if (!response.ok) {
                 throw new Error('HTTP error! Status: ' + response.status);
